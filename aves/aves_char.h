125453aa00018d3bdf9a23f2caac06aea66e3620
Adam Heurlin <adamheurlin@gmail.com>
2014-03-14 21:35:40 +0100

    [aves] Forgot to add the char files to the previous commit.

---
+++ aves/aves_char.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#ifndef AVES__CHAR_H
+#define AVES__CHAR_H
+
+#include "aves.h"
+#include "ov_unicode.h"
+
+class Char
+{
+public:
+	static LitString<2> ToLitString(const wuchar ch);
+};
+
+AVES_API NATIVE_FUNCTION(aves_Char_get_length);
+
+AVES_API NATIVE_FUNCTION(aves_Char_get_category);
+
+AVES_API NATIVE_FUNCTION(aves_Char_toUpper);
+AVES_API NATIVE_FUNCTION(aves_Char_toLower);
+
+AVES_API NATIVE_FUNCTION(aves_Char_getHashCode);
+AVES_API NATIVE_FUNCTION(aves_Char_toString);
+
+AVES_API NATIVE_FUNCTION(aves_Char_fromCodepoint);
+
+AVES_API NATIVE_FUNCTION(aves_Char_opEquals);
+AVES_API NATIVE_FUNCTION(aves_Char_opCompare);
+AVES_API NATIVE_FUNCTION(aves_Char_opMultiply);
+AVES_API NATIVE_FUNCTION(aves_Char_opPlus);
+
+#endif // AVES__CHAR_H
---
+++ aves/char.cpp
@@ -0,0 +1,156 @@
+#include "aves_char.h"
+#include "ov_string.h"
+#include "ov_stringbuffer.h"
+
+LitString<2> Char::ToLitString(const wuchar ch)
+{
+	LitString<2> output = {
+		ch > 0xFFFF ? 2 : 1,
+		0, StringFlags::STATIC
+	};
+
+	if (ch > 0xFFFF)
+	{
+		SurrogatePair pair = UC_ToSurrogatePair(ch);
+		output.chars[0] = pair.lead;
+		output.chars[1] = pair.trail;
+	}
+	else
+		output.chars[0] = (uchar)ch;
+
+	return output;
+}
+
+AVES_API NATIVE_FUNCTION(aves_Char_get_length)
+{
+	wuchar ch = (wuchar)THISV.integer;
+
+	VM_PushInt(thread, ch > 0xFFFF ? 2 : 1);
+}
+
+AVES_API NATIVE_FUNCTION(aves_Char_get_category)
+{
+	wuchar ch = (wuchar)THISV.integer;
+
+	Value cat;
+	cat.type = Types::UnicodeCategory;
+	cat.integer = (int32_t)UC_GetCategoryW(ch);
+
+	VM_Push(thread, cat);
+}
+
+AVES_API NATIVE_FUNCTION(aves_Char_toUpper)
+{
+	wuchar ch = (wuchar)THISV.integer;
+
+	Value upper;
+	upper.type = Types::Char;
+	upper.integer = (int32_t)UC_GetCaseMapW(ch).upper;
+
+	VM_Push(thread, upper);
+}
+AVES_API NATIVE_FUNCTION(aves_Char_toLower)
+{
+	wuchar ch = (wuchar)THISV.integer;
+
+	Value lower;
+	lower.type = Types::Char;
+	lower.integer = (int32_t)UC_GetCaseMapW(ch).lower;
+
+	VM_Push(thread, lower);
+}
+
+AVES_API NATIVE_FUNCTION(aves_Char_getHashCode)
+{
+	wuchar ch = (wuchar)THISV.integer;
+	LitString<2> str = Char::ToLitString(ch);
+
+	VM_PushInt(thread, String_GetHashCode(_S(str)));
+}
+AVES_API NATIVE_FUNCTION(aves_Char_toString)
+{
+	wuchar ch = (wuchar)THISV.integer;
+	LitString<2> str = Char::ToLitString(ch);
+
+	VM_PushString(thread, GC_ConstructString(thread, str.length, str.chars));
+}
+
+AVES_API NATIVE_FUNCTION(aves_Char_fromCodepoint)
+{
+	IntFromValue(thread, args + 0);
+
+	int64_t cp = args[0].integer;
+	if (cp < 0 || cp > 0x10FFFF)
+	{
+		VM_PushString(thread, strings::cp);
+		GC_Construct(thread, Types::ArgumentRangeError, 1, nullptr);
+		VM_Throw(thread);
+	}
+
+	Value character;
+	character.type = Types::Char;
+	character.integer = cp;
+	VM_Push(thread, character);
+}
+
+AVES_API NATIVE_FUNCTION(aves_Char_opEquals)
+{
+	bool eq;
+	if (args[1].type == Types::Char)
+		eq = args[0].integer == args[1].integer;
+	else if (args[1].type == Types::String)
+	{
+		LitString<2> left = Char::ToLitString((wuchar)args[0].integer);
+		eq = String_Equals(_S(left), args[1].common.string);
+	}
+	else
+		eq = false;
+
+	VM_PushBool(thread, eq);
+}
+AVES_API NATIVE_FUNCTION(aves_Char_opCompare)
+{
+	int result;
+
+	if (args[1].type == Types::Char)
+		result = (int32_t)args[0].integer - (int32_t)args[1].integer;
+	else if (args[1].type == Types::String)
+	{
+		LitString<2> left = Char::ToLitString((wuchar)args[0].integer);
+		result = String_Compare(_S(left), args[1].common.string);
+	}
+	else
+		VM_ThrowTypeError(thread);
+}
+AVES_API NATIVE_FUNCTION(aves_Char_opMultiply)
+{
+	IntFromValue(thread, args + 1);
+
+	int64_t times = args[1].integer;
+	if (times == 0)
+	{
+		VM_PushString(thread, strings::Empty);
+		return;
+	}
+
+	LitString<2> str = Char::ToLitString((wuchar)args[0].integer);
+	int64_t length = Int_MultiplyChecked(thread, times, str.length);
+	if (length > INT32_MAX)
+	{
+		VM_PushString(thread, strings::times);
+		GC_Construct(thread, Types::ArgumentRangeError, 1, nullptr);
+		VM_Throw(thread);
+	}
+
+	StringBuffer buf(thread, (int32_t)length);
+
+	for (int32_t i = 0; i < (int32_t)length; i++)
+		buf.Append(thread, _S(str));
+
+	String *result = buf.ToString(thread);
+	VM_PushString(thread, result);
+}
+AVES_API NATIVE_FUNCTION(aves_Char_opPlus)
+{
+	VM_PushInt(thread, args[0].integer);
+}
