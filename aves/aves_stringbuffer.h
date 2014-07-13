ec3c9f94e227d20a10d9b4ae5d2db978b87d549e
Adam Heurlin <adamheurlin@gmail.com>
2014-07-13 23:24:02 +0200

    [aves] * Added two classes, io.TextReader and io.TextWriter, which, respectively, read and write text from a stream. Like BinaryReader/Writer, but for characters! Some features:   - TextReader can read single characters, strings of specified lengths, and lines of text   - TextWriter has a configurable newline   - Both have a configurable encoding   - TextReader is currently *not* capable of detecting the encoding based on any BOM in the file, and TextWriter does not output a BOM (both of these are likely going to be implemented later) * Added documentation to a whole bunch of members in StringBuffer, and cleaned up the code a bit. * Added indexers to StringBuffer. * Improved the implementation of keepOpen in BinaryReader/Writer, and added checks for whether the reader/writer is closed to all read/write methods.

--- aves/aves_stringbuffer.h
+++ aves/aves_stringbuffer.h
@@ -10,14 +10,17 @@ AVES_API void aves_StringBuffer_init(TypeHandle type);
 AVES_API NATIVE_FUNCTION(aves_StringBuffer_new);
 AVES_API NATIVE_FUNCTION(aves_StringBuffer_newCap);
 
+AVES_API NATIVE_FUNCTION(aves_StringBuffer_get_item);
+AVES_API NATIVE_FUNCTION(aves_StringBuffer_set_item);
 AVES_API NATIVE_FUNCTION(aves_StringBuffer_get_length);
 AVES_API NATIVE_FUNCTION(aves_StringBuffer_get_capacity);
 
 AVES_API NATIVE_FUNCTION(aves_StringBuffer_appendLine);
-AVES_API NATIVE_FUNCTION(aves_StringBuffer_appendInternal);
-AVES_API NATIVE_FUNCTION(aves_StringBuffer_appendCodepointInternal);
-AVES_API NATIVE_FUNCTION(aves_StringBuffer_appendSubstrInternal);
-AVES_API NATIVE_FUNCTION(aves_StringBuffer_insertInternal);
+AVES_API NATIVE_FUNCTION(aves_StringBuffer_append);
+AVES_API NATIVE_FUNCTION(aves_StringBuffer_appendCodepoint);
+AVES_API NATIVE_FUNCTION(aves_StringBuffer_appendSubstrFromString);
+AVES_API NATIVE_FUNCTION(aves_StringBuffer_appendSubstrFromBuffer);
+AVES_API NATIVE_FUNCTION(aves_StringBuffer_insert);
 AVES_API NATIVE_FUNCTION(aves_StringBuffer_clear);
 AVES_API NATIVE_FUNCTION(aves_StringBuffer_toString);
 
