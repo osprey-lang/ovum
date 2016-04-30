4014122f4c51aa35f0fb894b8755760cb0d9f085
Adam Heurlin <adamheurlin@gmail.com>
2016-04-30 12:06:51 +0200

    [aves.tests] Build without /verbose

--- aves.tests/build.bat
+++ aves.tests/build.bat
@@ -5,4 +5,4 @@ set OSPC="%OSP%\Osprey\bin\Release\Osprey.exe"
 rem Path to the library folder
 set LIB=%OSP%\lib
 
-%OSPC% /libpath "%LIB%" /import testing.unit /main aves.tests.main /out aves.tests.ovm /verbose /r *.osp
+%OSPC% /libpath "%LIB%" /import testing.unit /main aves.tests.main /out aves.tests.ovm /r *.osp
